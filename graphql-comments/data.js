b1618721f52ed501685f55815a355e6ff09b2972
ahmetgursel <ahmetgursel@protonmail.com>
2022-06-27 02:04:01 +0300

    comment-challange

---
+++ graphql-comments/data.js
@@ -0,0 +1,61 @@
+const users = [
+  {
+    id: "1",
+    fullName: "Mehmet Seven",
+  },
+  {
+    id: "2",
+    fullName: "Ahmet Gursel",
+  },
+];
+
+const posts = [
+  {
+    id: "1",
+    title: "Mehmet'in gonderisi",
+    user_id: "1",
+  },
+  {
+    id: "2",
+    title: "Mehmet'in diger gonderisi",
+    user_id: "1",
+  },
+  {
+    id: "3",
+    title: "Ahmet'in gonderisi",
+    user_id: "2",
+  },
+];
+
+const comments = [
+  {
+    id: "1",
+    text: "Bu Ahmet'in yorumudur.",
+    post_id: "1",
+    user_id: "2",
+  },
+  {
+    id: "2",
+    text: "Bu Mehmet'in yorumudur.",
+    post_id: "1",
+    user_id: "1",
+  },
+  {
+    id: "3",
+    text: "Bu Ahmet'in yorumudur.",
+    post_id: "2",
+    user_id: "2",
+  },
+  {
+    id: "4",
+    text: "Bu Mehmet'in yorumudur.",
+    post_id: "3",
+    user_id: "1",
+  },
+];
+
+module.exports = {
+  users,
+  posts,
+  comments,
+};
---
+++ graphql-comments/index.js
@@ -0,0 +1,108 @@
+const { ApolloServer, gql } = require("apollo-server");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const { users, posts, comments } = require("./data");
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    fullName: String!
+    posts: [Post!]!
+    comment: [Comment!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    user_id: ID!
+    user: User!
+    comment: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    post_id: ID!
+    user: User!
+    post: Post!
+  }
+
+  type Query {
+    #users
+    users: [User!]!
+    user(id: ID!): User!
+
+    #posts
+    posts: [Post!]!
+    post(id: ID!): Post!
+
+    #comments
+    comments: [Comment!]!
+    comment(id: ID!): Comment!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    // user
+    users: () => users,
+    user: (parent, args) => {
+      const user = users.find((user) => user.id === args.id);
+      if (!user) {
+        return new Error("User not found!");
+      } else {
+        return user;
+      }
+    },
+
+    // post
+    posts: () => posts,
+    post: (parent, args) => {
+      const post = posts.find((post) => post.id === args.id);
+      if (!post) {
+        return new Error("Post not found!");
+      } else {
+        return post;
+      }
+    },
+
+    // comments
+    comments: () => comments,
+    comment: (parent, args) => {
+      const comment = comments.find((comment) => comment.id === args.id);
+      if (!comment) {
+        return new Error("Comment not found!");
+      } else {
+        return comment;
+      }
+    },
+  },
+
+  User: {
+    posts: (parent) => posts.filter((post) => post.user_id === parent.id),
+    comment: (parent) =>
+      comments.filter((comment) => comment.user_id === parent.id),
+  },
+
+  Post: {
+    user: (parent) => users.find((user) => user.id === parent.user_id),
+    comment: (parent) =>
+      comments.filter((comment) => comment.post_id === parent.id),
+  },
+
+  Comment: {
+    user: (parent) => users.find((user) => user.id === parent.user_id),
+    post: (parent) => posts.find((post) => post.id === parent.post_id),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is ready at ${url}`);
+});

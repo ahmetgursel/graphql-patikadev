51f8a8b3bccf7ad20f8ee6fe88da87f2a15d8f4c
ahmetgursel <ahmetgursel@protonmail.com>
2022-06-28 05:50:16 +0300

    basic mutation implementation

--- graphql-comments/index.js
+++ graphql-comments/index.js
@@ -1,8 +1,9 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require('apollo-server');
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
-} = require("apollo-server-core");
-const { users, posts, comments } = require("./data");
+} = require('apollo-server-core');
+const { nanoid } = require('nanoid');
+const { users, posts, comments } = require('./data');
 
 const typeDefs = gql`
   type User {
@@ -41,6 +42,12 @@ const typeDefs = gql`
     comments: [Comment!]!
     comment(id: ID!): Comment!
   }
+
+  type Mutation {
+    createUser(fullName: String!): User!
+    createPost(title: String!, user_id: ID!): Post!
+    createComment(text: String!, post_id: ID!, user_id: ID!): Comment!
+  }
 `;
 
 const resolvers = {
@@ -50,7 +57,7 @@ const resolvers = {
     user: (parent, args) => {
       const user = users.find((user) => user.id === args.id);
       if (!user) {
-        return new Error("User not found!");
+        return new Error('User not found!');
       } else {
         return user;
       }
@@ -61,7 +68,7 @@ const resolvers = {
     post: (parent, args) => {
       const post = posts.find((post) => post.id === args.id);
       if (!post) {
-        return new Error("Post not found!");
+        return new Error('Post not found!');
       } else {
         return post;
       }
@@ -72,7 +79,7 @@ const resolvers = {
     comment: (parent, args) => {
       const comment = comments.find((comment) => comment.id === args.id);
       if (!comment) {
-        return new Error("Comment not found!");
+        return new Error('Comment not found!');
       } else {
         return comment;
       }
@@ -94,6 +101,36 @@ const resolvers = {
   Comment: {
     user: (parent) => users.find((user) => user.id === parent.user_id),
     post: (parent) => posts.find((post) => post.id === parent.post_id),
+  },
+
+  Mutation: {
+    createUser: (parent, { fullName }) => {
+      const user = { id: nanoid(), fullName: fullName };
+      users.push(user);
+
+      return user;
+    },
+    createPost: (parent, { title, user_id }) => {
+      const post = {
+        id: nanoid(),
+        title: title,
+        user_id: user_id,
+      };
+
+      posts.push(post);
+      return post;
+    },
+    createComment: (parent, { text, post_id, user_id }) => {
+      const comment = {
+        id: nanoid(),
+        text: text,
+        post_id: post_id,
+        user_id: user_id,
+      };
+
+      comments.push(comment);
+      return comment;
+    },
   },
 };
 
